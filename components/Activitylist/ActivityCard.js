5bcf9c95113103d293a3b1e4fbf30c4670d4420e
Anna <awoellnitz@web.de>
2025-12-02 11:08:48 +0100

    updated2_task1

--- components/Activitylist/ActivityCard.js
+++ components/Activitylist/ActivityCard.js
@@ -2,20 +2,13 @@ import Image from "next/image";
 
 export default function ActivityCard({ title, area, country, description }) {
   return (
-    <>
-      <li>
-        <article>
-          <h3>{title}</h3>
-        </article>
-        <article>
-          <p>{description}</p>
-        </article>
-        <ul>
-          <li>{area}</li>
-          <li>{country}</li>
-        </ul>
-        <p></p>
-      </li>
-    </>
+    <article>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <ul>
+        <li>{area}</li>
+        <li>{country}</li>
+      </ul>
+    </article>
   );
 }
