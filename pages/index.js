c5d45378dcc8e7ce849f1103268f0579efd2b6e5
Elisa Linnemannstoens <elisalin2001@gmail.com>
2025-12-02 12:21:26 +0100

    added Link to MainPage

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
+//dynamischer Link kann nach der pull request erstellt werden
+
 export default function HomePage() {
   return (
     <div>
       <h1>Hello from Next.js</h1>
-      
+      <Link href="/activities/Surfing">Go to Surfing Activity</Link>
     </div>
   );
 }
