3bc08a8b3c071fbf49cf128341f7e29c468f8a56
Elisa Linnemannstoens <elisalin2001@gmail.com>
2025-12-02 10:19:14 +0100

    added styled-components and rearranged folder structure

--- components/Activity-Details/ActivityDetails.js
+++ components/Activity-Details/ActivityDetails.js
@@ -13,14 +13,19 @@ export default function ActivityDetails({ activity }) {
       <Link href="/">
         <BackButton />
       </Link>
-      <Image src={activity.imageUrl} alt={activity.title} height={300} width={300} />
+      <Image
+        src={activity.imageUrl}
+        alt={activity.title}
+        height={300}
+        width={300}
+      />
       <p>{activity.description}</p>
       <p>Area: {activity.area}</p>
       <p>Country: {activity.country}</p>
       {activity.categories && activity.categories.length > 0 && (
         <div>
           <strong>Categories:</strong>
-          {activity.categories.map(category => (
+          {activity.categories.map((category) => (
             <span key={category._id}> {category.name}</span>
           ))}
         </div>
