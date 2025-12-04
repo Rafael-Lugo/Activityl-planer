21f8a55dfcae3430c4afd8da85d668d1e544ec63
Elisa Linnemannstoens <elisalin2001@gmail.com>
2025-12-04 12:29:53 +0100

    rearanged link

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -5,7 +5,7 @@ export default function Navigation (){
         <nav>
             <ul>
                 <li><Link href="createactivity">Add</Link></li>
-                 <li><Link href="/"></Link>Home</li>
+                 <li><Link href="/">Home</Link></li>
                   <li><Link href="bookmark">Bookmark</Link></li>
                  
             </ul>
