ac051479fb61c0290944856121cfa863aa299661
Elisa Linnemannstoens <elisalin2001@gmail.com>
2025-12-04 15:59:46 +0100

    change on Home-Link

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import { NavigationLink, NavigationList, NavigationListItem, NavigationWrapper } from "./StyledNavigation"
 import router, { Router } from "next/router"
 
@@ -5,10 +6,8 @@ export default function Navigation (){
     return(
         <NavigationWrapper>
             <NavigationList>
-                <NavigationListItem><NavigationLink href="createactivity" $highlighted={router.pathname === "/createactivity"}>Add</NavigationLink></NavigationListItem>
-                 <NavigationListItem><NavigationLink href="/"$highlighted={router.pathname === "/"}>Home</NavigationLink></NavigationListItem>
-                 
-                 
+                <NavigationListItem><NavigationLink src="@src/add.svg" href="createactivity" $highlighted={router.pathname === "/createactivity"}>Add</NavigationLink></NavigationListItem>
+                <NavigationListItem><NavigationLink href="/"$highlighted={router.pathname === "/"}>Home</NavigationLink></NavigationListItem>
             </NavigationList>
         </NavigationWrapper>
     )
