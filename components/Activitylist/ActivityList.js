74cc703e7dd54aa1b2e7711594cfbcca3dd19fe7
Anna <awoellnitz@web.de>
2025-12-04 12:16:47 +0100

    filterfunction

--- components/Activitylist/ActivityList.js
+++ components/Activitylist/ActivityList.js
@@ -1,18 +1,13 @@
-import useSWR from "swr";
 import ActivityCard from "./ActivityCard";
 
-export default function ActivityList() {
-  const { data, isLoading, error } = useSWR("/api/activities");
-
-  if (isLoading) return <p>Loading...</p>;
-
-  if (error) return <p>Error to load the Page.</p>;
+export default function ActivityList({ activities }) {
+  if (!activities) return <p>No activities found.</p>;
 
   return (
     <>
       <h3>Activities:</h3>
       <ul>
-        {data.map((activity) => (
+        {activities.map((activity) => (
           <ActivityCard key={activity._id} {...activity} />
         ))}
       </ul>
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,42 @@
 import ActivityCard from "@/components/Activitylist/ActivityCard";
 import ActivityList from "@/components/Activitylist/ActivityList";
+import useSWR from "swr";
+import { useState } from "react";
 
 export default function HomePage() {
+  const { data: activities, isLoading, error } = useSWR("/api/activities");
+  const [search, setSearch] = useState("");
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error to load the Page.</p>;
+
+  const filterActivities = activities.filter((activity) => {
+    const match = search.toLowerCase();
+
+    const title = activity.title?.toLowerCase() || "";
+    const category = activity.category?.toLowerCase() || "";
+    const country = activity.country?.toLowerCase() || "";
+
+    return (
+      title.includes(match) ||
+      category.includes(match) ||
+      country.includes(match)
+    );
+  });
+
+  function handleSearch(event) {
+    const inputElement = event.target;
+    const newValue = inputElement.value;
+
+    setSearch(newValue);
+  }
+
   return (
     <>
       <h1>Activity Planer</h1>
       <h2>for your next journey</h2>
-      <ActivityList />
+      <input placeholder="Filter: Title, Category, Country" value={search} onChange={handleSearch} />
+      <ActivityList activities={filterActivities} />
       <ActivityCard />
     </>
   );
