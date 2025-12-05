8fe7c4377a818dc0d16984461a3e32b0b8232f68
Rafael Lugo <rafaelugo@gmx.de>
2025-12-05 10:22:53 +0100

    style update

--- components/Activitylist/ActivityCard.js
+++ components/Activitylist/ActivityCard.js
@@ -1,22 +1,25 @@
 import Link from "next/link";
 
-export default function ActivityCard({ _id, title, area, country, description, imageUrl }) {
+export default function ActivityCard({
+  _id,
+  title,
+  country,
+  categories,
+  area,
+  imageUrl
+}) {
   return (
     <li>
       <Link href={`/activities/${_id}`}>
-       <img
-              src={imageUrl}
-              alt={title}
-              height={300}
-              width={300}
-            />
-      <h3>{title}</h3>
+        <img src={imageUrl} alt={title} height={300} width={300} />
       </Link>
-      <p>{description}</p>
+      <h3>{title}</h3>
       <ul>
         <li>{area}</li>
         <li>{country}</li>
       </ul>
+      {categories &&
+        categories.map((category) => <p key={category._id}>{category.name}</p>)}
     </li>
   );
 }
