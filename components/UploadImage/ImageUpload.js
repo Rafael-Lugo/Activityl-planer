e1e96f7d8a660377904d2d01ce285809630abbb6
Elisa Linnemannstoens <elisalin2001@gmail.com>
2025-12-08 18:22:14 +0100

    finished UploadImage Component

---
+++ components/UploadImage/ImageUpload.js
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import { useState } from "react";
+
+export default function ImageUpload({ onFileSelect }) {
+  const [preview, setPreview] = useState(null);
+
+  function handleFileChange(event) {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const previewURL = URL.createObjectURL(file);
+    setPreview(previewURL);
+
+    // Call the callback function to pass the file to the parent component
+    if (onFileSelect) {
+      onFileSelect(file);
+    }
+  }
+
+  return (
+    <>
+      <input
+        type="file"
+        name="cover"
+        accept="image/*"
+        required
+        onChange={handleFileChange}
+      />
+      {preview && (
+        <img
+          src={preview}
+          alt="Preview"
+          style={{ width: "200px", marginTop: "10px" }}
+        />
+      )}
+    </>
+  );
+}
+
+const StyledInput = styled.input`
+  padding: 8px;
+  width: 300px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  &:focus {
+    border-color: #0056b3;
+    outline: none;
+  }
+`;
+
+const StyledFileInput = styled(StyledInput).attrs({
+  type: "file",
+})`
+  padding: 10px;
+  border: none;
+  &:focus {
+    border-color: #0056b3;
+    outline: none;
+  }
+`;
