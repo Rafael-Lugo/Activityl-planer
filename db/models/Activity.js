96c7a4ef31243ab0d372a8606721eb33d21179bd
Elisa Linnemannstoens <elisalin2001@gmail.com>
2025-12-08 18:30:23 +0100

    Updated API Endpoint to create URL

--- db/models/Activity.js
+++ db/models/Activity.js
@@ -5,7 +5,11 @@ const { Schema } = mongoose;
 
 const activitySchema = new Schema({
   title: { type: String, required: true, minlength: 3 },
-  imageUrl: { type: String, required: true, default: "/placeholder.png" },
+  imageUrl: {
+    width: { type: String, required: true },
+    height: { type: String, required: true },
+    url: { type: String, required: true },
+  },
   categories: { type: [Schema.Types.ObjectId], ref: "Category" },
   description: { type: String },
   area: { type: String },
