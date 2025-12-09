74f1fb8f5d0928a1c321f6e5ea36a9dce3bf58ce
Efstathios <modo575@hotmail.com>
2025-12-09 11:37:13 +0100

    add LoginComponent.js and logic2

--- components/Login/LoginComponent.js
+++ components/Login/LoginComponent.js
@@ -1,8 +1,13 @@
-import { signOut, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function LoginComponent() {
   const { data: session } = useSession();
   if (session) {
     return <button onClick={() => signOut()}>Logout</button>;
   }
+  return (
+    <button onClick={() => signIn("github", { callbackUrl: "/" })}>
+      Login with GitHub
+    </button>
+  );
 }
