1be2f5f3288879599331d15deb0c1f28a09637e9
Rafael Lugo <rafaelugo@gmx.de>
2025-12-12 09:16:42 +0100

    fix

--- components/Weather-Activity/Weather-Activity.js
+++
@@ -1,9 +0,0 @@
-import useSWR from "swr"
-
-const fetcher = (url) => fetch(url).then((response) => response.json());
-
-export default function WeatherActivity(){
-    const { data } = useSWR()
-    
-    return ()
-}
